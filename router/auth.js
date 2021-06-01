c52bf949c4661b31b12df38c9beac728e01dcc0c
FELICIANO <marcelo.feliciano2@fatec.sp.gov.br>
2021-06-01 02:49:17 -0300

    Created table users

--- router/auth.js
+++ router/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const authController = require('../controllers/auth')
+const authController = require('../mvc/auth')
 const router = express.Router();
 
 router.post('/register', authController.register);
